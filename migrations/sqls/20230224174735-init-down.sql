3a0010e14b50b05c785f0851705ad1986589fd2f
nevezide <nevezide44@gmail.com>
2024-04-05 16:58:00 +0200

    Merge library db migration script

--- migrations/sqls/20230224174735-init-down.sql
+++ migrations/sqls/20230224174735-init-down.sql
@@ -1,2 +1,10 @@
 
 DROP TABLE "User";
+DROP TABLE "Boutique";
+DROP TABLE "Film";
+DROP TABLE "Realisateur";
+DROP TABLE "Livre_Auteur";
+DROP TABLE "Livre";
+DROP TABLE "Editeur";
+DROP TABLE "Auteur";
+DROP TABLE "Langue";
